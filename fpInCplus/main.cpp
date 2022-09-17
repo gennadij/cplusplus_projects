95810be27748817c18b86871dcc335bf52db55b0
Gennadi Heimann <genaheimann@gmail.com>
2022-09-17 23:06:35 +0200

    trim

--- fpInCplus/main.cpp
+++ fpInCplus/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "chapter2.cpp"
 #include <vector>
 #include <algorithm>
 #include <numeric>
@@ -8,6 +7,19 @@ double average_score(const std::vector<int>& scores){
   return std::accumulate(scores.begin(), scores.end(), 0) / (double) scores.size();
 }
 
+std::string trim_left(std::string s){
+  s.erase(s.begin(), find_if(s.begin(), s.end(), [](char ss){return ss != ' ';}));
+  return s;
+}
+
+std::string trim_right(std::string s){
+  s.erase(find_if(s.begin(), s.end(), [](char ss){return ss != ' ';}), s.end());
+  return s;
+}
+
+std::string trim(std::string s){
+  return trim_left(trim_right(s));
+}
 
 int main(int, char**) {
   // chapter2
@@ -15,6 +27,11 @@ int main(int, char**) {
 
   std::cout << "Average " << average_score(avg) << "\n";
 
+  std::cout << "Trim left :" << trim_left("     TrimLeft") << "\n";
+
+  std::cout << "Trim right :" << trim_left("TrimRight     ") << "\n";
+
+  std::cout << "Trim :" << trim_left("      Trim     ") << "\n";
 }
 
 
