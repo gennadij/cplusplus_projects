955006c6e38affd2d031b9e91cf4b876ca84d2e7
Gennadi Heimann <genaheimann@gmail.com>
2022-09-17 23:51:45 +0200

    partition

--- fpInCplus/main.cpp
+++ fpInCplus/main.cpp
@@ -3,6 +3,9 @@
 #include <algorithm>
 #include <numeric>
 
+using std::string;
+using std::vector;
+
 double average_score(const std::vector<int>& scores){
   return std::accumulate(scores.begin(), scores.end(), 0) / (double) scores.size();
 }
@@ -21,6 +24,33 @@ std::string trim(std::string s){
   return trim_left(trim_right(s));
 }
 
+struct People {
+  string name;
+  string gender;
+};
+
+vector<People> initReople() {
+  return vector<People> {
+    People{"Marta", "female"}, 
+    People{"Peter", "male"},
+    People{"Sandra", "female"},
+    People{"Lucy", "female"},
+    People{"Mark", "male"},
+    People{"Anton", "male"},
+    People{"Luda", "female"},
+    People{"Artiom", "male"},
+    People{"Aljona", "female"}
+    }; 
+}
+
+bool is_female(const People& p){
+  return p.gender == "female";
+}
+
+bool is_not_female(const People& p){
+  return p.gender != "female";
+}
+
 int main(int, char**) {
   // chapter2
   std::vector<int> avg{1,2,3,4,5};
@@ -32,6 +62,55 @@ int main(int, char**) {
   std::cout << "Trim right :" << trim_left("TrimRight     ") << "\n";
 
   std::cout << "Trim :" << trim_left("      Trim     ") << "\n";
+
+  // chapter 2.2.4
+  //=========================================================================
+
+  vector<People> people1 = initReople();
+  
+  auto isFemale = std::partition(people1.begin(), people1.end(), is_female);
+  
+  std::cout << "people1";
+  
+  std::for_each(
+    people1.begin(), people1.end(), 
+    [](const People& p){std::cout << " " << p.name << " ";});
+  
+  //=========================================================================
+
+  vector<People> people2 = initReople();
+
+  auto isNotFemale = std::partition(people2.begin(), people2.end(), is_not_female); 
+  
+  std::cout << "\n people2";
+  
+  std::for_each(
+    people2.begin(), people2.end(), 
+    [](const People& p){std::cout << " " << p.name << " ";});
+
+  //=========================================================================
+
+  vector<People> people3 = initReople();
+  
+  std::stable_partition(people3.begin(), people3.end(), is_female);
+
+  std::cout << "\n people3";
+
+  std::for_each(
+    people3.begin(), people3.end(), 
+    [](const People& p){std::cout << " " << p.name << " ";});
+
+  //=========================================================================
+  
+  vector<People> people4 = initReople();
+
+  std::stable_partition(people4.begin(), people4.end(), is_not_female);
+  
+  std::cout << "\n people4";
+  
+  std::for_each(
+    people4.begin(), people4.end(), 
+    [](const People& p){std::cout << " " << p.name << " ";});
 }
 
 
