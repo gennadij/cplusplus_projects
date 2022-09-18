96d7e560f2354dab98a1337ce739a0df1b6e4c0d
Gennadi <gennadiheimann@gmail.com>
2022-09-18 21:09:15 +0200

    Trim

--- fpInCplus/main.cpp
+++ fpInCplus/main.cpp
@@ -3,9 +3,15 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
-
+#include <cctype>
 double average_score(const std::vector<int>& scores){
   return std::accumulate(scores.begin(), scores.end(), 0) / (double) scores.size();
+}
+bool is_not_space(char c){
+  !isspace(c);
+}
+std::string trimLeft(std::string s){
+  s.erase(s.begin(), std::find_if(s.begin(), s.end(), is_not_space));
 }
 
 
@@ -14,6 +20,11 @@ int main(int, char**) {
   std::vector<int> avg{1,2,3,4,5};
 
   std::cout << "Average " << average_score(avg) << "\n";
+
+  std::string trim = "  spacesLeft";
+
+  std::cout << "Trim Left " << trimLeft(trim) << "\n";
+  
 
 }
 
